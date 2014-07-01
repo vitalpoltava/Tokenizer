ed96e92e65ef96214221f8f444f86bfe8c2f4b9e
vitalpoltava <omelkin@gmail.com>
2014-07-01 12:27:49 +0300

    optimize tokenz list rendering

--- js/tokenizer.js
+++ js/tokenizer.js
@@ -15,37 +15,31 @@
                 };
 
                 function isTokenUnique(token) {
-                    return $scope.tokensArr.indexOf(token) !== -1 ? false : token;
+                    return $scope.tokens.indexOf(token) !== -1 ? false : token;
                 };
 
-                $scope.tokensArr = [];
+                $scope.tokens = [];
                 $scope.tokensStoreDivider = ';';
 
                 // here we starts to handle all inputs
                 $scope.handleInputKeyup = function(event) {
                     if (_isEnterPressed(event)) {
                         $scope.addTokenToModel();
-                        $scope.renderTokensList();
                         $scope.updateTokensStoreElement();
                     }
                 };
 
                 $scope.addTokenToModel = function() {
-                    if ($scope.nonUniqueTokensAllowed) {
-                        $scope.tokensArr.push($scope.token);
-                    } else {
-                        $scope.tokensArr.push(isTokenUnique($scope.token));
+                    if ($scope.nonUniqueTokensAllowed || isTokenUnique($scope.token)) {
+                        $scope.tokens.push($scope.token);
                     }
 
                     $scope.token = ''; // clear input
                 };
 
                 $scope.closeToken = function(index) {
-                    if (angular.isNumber(index) && $scope.tokensArr[index]) {
-                        $scope.tokensArr[index] = false;
-                        $scope.renderTokensList();
-                        $scope.updateTokensStoreElement();
-                    }
+                    $scope.tokens.splice(index, 1);
+                    $scope.updateTokensStoreElement();
                 };
             },
 
@@ -59,31 +53,15 @@
                 element.append(tokensStoreEl);
                 element.append(tokensInput);
 
-                scope.renderTokensList = function() {
-                    var renderList = function(token, index) {
-                        if (!token) return;
-                        var tokenTmpl = $templateCache.get('Tokenizer.token.partials.tmpl.html')
-                            .replace("[token]", token)
-                            .replace("[index]", index);
-                        scope.tokensHtml += tokenTmpl;
-                    };
-
-                    scope.tokensHtml = ''; // init tokens HTML
-                    scope.tokensArr.forEach(renderList); // render list
-                    tokensContainer.html($compile(angular.element(scope.tokensHtml))(scope)); // add tokens to DOM
-                    tokensInput.focus();
-                };
-
                 // form a list of selected tokens for outer use
                 scope.updateTokensStoreElement = function() {
-                    var truthy = function(el) {return el;};
-                    tokensStoreEl.val(scope.tokensArr.filter(truthy).join(scope.tokensStoreDivider));
+                    tokensStoreEl.val(scope.tokens.join(scope.tokensStoreDivider));
                 };
 
                 // Apply initial system values
                 // ---------------------------
                 // 1. Placeholder
-                tokensInput.attr('placeholder', attrs.placeholder || 'Input token');
+                scope.placeholder = attrs.placeholder || 'Input token';
                 // 2. Main wrapper  class
                 scope.wrapperClass = attrs.wrapperClass || 'TokenizerWrapper';
                 // 3. Tokens area  class
